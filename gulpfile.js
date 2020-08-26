0c0667d6b1e231bb0f98b2367ea1078df70abcdc
Konovalov Alexander <konovalov_a.s@mail.ru>
2020-08-26 14:23:33 +0500

    gh-pages

--- gulpfile.js
+++ gulpfile.js
@@ -17,6 +17,7 @@ var include = require("posthtml-include");
 var del = require("del");
 var uglify = require('gulp-uglify');
 var htmlValidator = require('gulp-w3c-html-validator');
+var ghPages = require('gulp-gh-pages');
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
@@ -113,5 +114,13 @@ gulp.task("clean", function () {
   return del("build");
 });
 
+gulp.task("deploy", function() {
+  return gulp.src("build/**/*")
+    .pipe(ghPages());
+});
+
 gulp.task("build", gulp.series("clean", "copy", "css", "js", "sprite", "html"));
 gulp.task("start", gulp.series("build", "server"));
+
+
+
